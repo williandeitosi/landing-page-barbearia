f3539d81d35c017b0ecd01c90ecca1ec28f78a35
willian <williandeitosi@gmail.com>
2024-06-26 21:03:25 -0300

    feat: table hours

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import salon from './assets/baberSalon.jpg';
 import skull from './assets/skullBg.png';
 import { About } from './components/About/About';
 import { Header } from './components/Heeader/Header';
+import { Hours } from './components/Hours/Hours';
 import { ImgSalon } from './components/ImgSalon/ImgSalon';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <ImgSalon bgimg={salon} bgalt='salon' img={skull} alt='logo' />
       <About />
+      <Hours />
     </div>
   );
 }
