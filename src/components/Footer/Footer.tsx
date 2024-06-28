945309614304cdd31d319ebd3796857200200925
willian <williandeitosi@gmail.com>
2024-06-28 06:46:19 -0300

    feat(footer): responsive for small screen

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,10 +2,13 @@ import { InstagramLogo, XLogo } from '@phosphor-icons/react';
 
 export function Footer() {
   return (
-    <footer className='bg-zinc-900 flex justify-between items-center p-8'>
-      <p className='text-white text-xl'>@2024 Barbearia</p>
-      <p className='text-white text-xl'>
-        Rua: fictícia - Bairro: fictícia - N: 99999 Tel: (99) 99999-9999
+    <footer className='bg-zinc-900 flex-col lg:flex-row flex justify-between items-center p-8'>
+      <p className='text-white text-xl mb-3'>@2024 Barbearia</p>
+      <p className='text-white text-xl flex gap-3 flex-col md:flex-row md:gap-5 justify-center items-center mb-3'>
+        <span>Rua: fictícia</span>
+        <span>Bairro: fictícia</span>
+        <span>N: 99999</span>
+        <span>Tel: (99) 99999-9999</span>
       </p>
       <div className='flex gap-3'>
         <a href='#'>
